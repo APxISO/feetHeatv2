b360f36a1c04f2ad4e2b8478e9961023d5e7b2f4
Adam Perez <Adam@Domoniques-MBP.hsd1.fl.comcast.net>
2023-12-21 12:09:03 -0500

    db config

--- db/index.js
+++ db/index.js
@@ -3,28 +3,30 @@ const { Pool } = require('pg');
 
 let dbConfig = {};
 
+// Check if the app is running on Heroku
 if (process.env.DATABASE_URL) {
-  dbConfig = {
-    connectionString: process.env.DATABASE_URL,
-    ssl: {
-      rejectUnauthorized: false
-    }
-  };
+    // Configure the database connection for Heroku PostgreSQL
+    dbConfig = {
+        connectionString: process.env.DATABASE_URL,
+        ssl: {
+            rejectUnauthorized: false // Required for Heroku PostgreSQL connection
+        }
+    };
 } else {
-  // Local database configuration
-  dbConfig = {
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    host: 'localhost',
-    port: 5432,
-    database: process.env.DB_NAME
-  };
+    // Local database configuration
+    dbConfig = {
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        host: 'localhost',
+        port: 5432,
+        database: process.env.DB_NAME
+    };
 }
 
 const db = new Pool(dbConfig);
 
 db.connect()
-  .then(() => console.log("Connected to database"))
-  .catch(err => console.error("Database connection error", err));
+    .then(() => console.log("Connected to database"))
+    .catch(err => console.error("Database connection error", err));
 
 module.exports = db;
